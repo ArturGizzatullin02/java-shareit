69ef5c1d2c5c25bdcc356ff3bd388a0edb170f24
ArturGizzatullin02 <salatikufa@yandex.ru>
2024-11-12 00:08:09 +0500

    изменены настройки БД

--- src/test/java/ru/practicum/shareit/ShareItTests.java
+++ src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -2,9 +2,8 @@ package ru.practicum.shareit;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import ru.practicum.shareit.config.H2JpaConfig;
 
-@SpringBootTest(classes = {ShareItApp.class, H2JpaConfig.class})
+@SpringBootTest
 class ShareItTests {
 
     @Test
