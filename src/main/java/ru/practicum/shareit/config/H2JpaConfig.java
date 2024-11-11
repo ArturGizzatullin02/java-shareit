94e245b32f80465f5e33832e711370aac21d39d5
ArturGizzatullin02 <salatikufa@yandex.ru>
2024-11-11 22:50:44 +0500

    добавлен конфиг для H2, подключен в тестах

---
+++ src/main/java/ru/practicum/shareit/config/H2JpaConfig.java
@@ -0,0 +1,13 @@
+package ru.practicum.shareit.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableJpaRepositories(basePackages = "ru.practicum.shareit")
+@PropertySource("application-test.properties")
+@EnableTransactionManagement
+public class H2JpaConfig {
+}
--- src/test/java/ru/practicum/shareit/ShareItTests.java
+++ src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -2,8 +2,9 @@ package ru.practicum.shareit;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import ru.practicum.shareit.config.H2JpaConfig;
 
-@SpringBootTest
+@SpringBootTest(classes = {ShareItApp.class, H2JpaConfig.class})
 class ShareItTests {
 
     @Test
